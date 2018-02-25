382f653355aaa7de33e9dcfdced221628a4c34fc
Yijun Pan <31780386+evelyn6666@users.noreply.github.com>
2018-02-25 04:13:15 -0500

    Add files via upload

---
+++ description.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net.Http;
+using System.Web;
+using Newtonsoft.Json.Linq;
+
+namespace test2
+{
+    class Class1
+
+    {
+        static void Main()
+        {
+            MakeRequest();
+            Console.WriteLine("Hit ENTER to exit...");
+            Console.ReadLine();
+        }
+
+        static async void MakeRequest()
+        {
+            var client = new HttpClient();
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+
+            // Request headers
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "dd2d63038e6149cf923777ec190f1190");
+
+            // Request parameters
+            queryString["q"] = "pheonix claws";
+            queryString["count"] = "15";
+            queryString["offset"] = "0";
+            queryString["mkt"] = "en-us";
+            queryString["safeSearch"] = "Moderate";
+            var uri = "https://api.cognitive.microsoft.com/bing/v7.0/search?" + queryString;
+
+            var response = await client.GetAsync(uri);
+            if (response.Content != null)
+            {
+                var responseString = await response.Content.ReadAsStringAsync();
+
+                JObject mainObject = JObject.Parse(responseString);
+                JObject webPages = (JObject) mainObject["webPages"];
+                JArray data = (JArray)webPages["value"];
+                for (int i = 0; i < data.Count; i++)
+                {
+                    string name = data[i]["name"].ToObject<string>();
+
+
+                    if (name.Contains("Wikipedia") == true)
+                    {
+                        Console.WriteLine(data[i]["snippet"].ToObject<string>());
+                    }
+                }
+
+            }
+        }
+    }
+}
---
+++ translator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Http;
+using System.Web;
+using Newtonsoft.Json.Linq;
+
+namespace translator
+{
+    class translator
+    {
+        static void Main()
+        {
+            MakeRequest();
+            Console.WriteLine("Hit ENTER to exit...");
+            Console.ReadLine();
+        }
+
+        static async void MakeRequest()
+        {
+            var client = new HttpClient();
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+            // Request parameters
+            /*queryString["key"] = "trnsl.1.1.20180225T070421Z.f408b50a72b55847.adcaec1e142691d34c576dae138acc6145c9adf5";
+            queryString["text"] = "凤爪";
+            queryString["lang"] = "zh-en";*/
+            string text = "凤爪";
+            var uri = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20180225T070421Z.f408b50a72b55847.adcaec1e142691d34c576dae138acc6145c9adf5&text="+text+"&lang=zh-en";
+            var response = await client.GetAsync(uri);
+            if (response.Content != null)
+            {
+                var responseString = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(responseString);              
+            }
+        }
+    }
+}
